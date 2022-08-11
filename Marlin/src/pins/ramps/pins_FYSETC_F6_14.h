6baac4ad8ec9f85599937e84c7f6a9b0e3994e78
Roman Lubij <roman.lubij@gmail.com>
2022-08-11 14:31:15 +0200

    test

--- Marlin/src/pins/ramps/pins_FYSETC_F6_14.h
+++ Marlin/src/pins/ramps/pins_FYSETC_F6_14.h
@@ -29,7 +29,7 @@
 
 #define Z_MAX_PIN                              2
 
-#if HAS_TMC_UART
+//#if HAS_TMC_UART
   /**
    * TMC2208/TMC2209 stepper drivers
    */
@@ -50,25 +50,11 @@
 
   #define E2_SERIAL_TX_PIN                    82
   #define E2_SERIAL_RX_PIN                    62
-/*
-  #define X_SERIAL_RX_PIN       71
-  #define X_SERIAL_TX_PIN       72
 
-  #define Y_SERIAL_RX_PIN       73
-  #define Y_SERIAL_TX_PIN       75
-
-  #define Z_SERIAL_RX_PIN       78
-  #define Z_SERIAL_TX_PIN       79
-
-  #define E0_SERIAL_RX_PIN      76
-  #define E0_SERIAL_TX_PIN      77
-
-  #define E1_SERIAL_RX_PIN      80
-  #define E1_SERIAL_TX_PIN      81
-
-  #define E2_SERIAL_RX_PIN      22
-  #define E2_SERIAL_TX_PIN      82
-*/
-#endif
+  #define X_HARDWARE_SERIAL  Serial1
+  #define Y_HARDWARE_SERIAL  Serial1
+  #define Z_HARDWARE_SERIAL  Serial1
+  #define E0_HARDWARE_SERIAL Serial1
+//#endif
 
 #include "pins_FYSETC_F6_13.h"
