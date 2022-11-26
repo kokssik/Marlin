58b4757555908c41a7452150bbc6a87627868315
Roman Lubij <roman.lubij@gmail.com>
2022-11-26 14:35:52 +0100

    config tweak yet again

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2096,7 +2096,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (6*60) }
+#define HOMING_FEEDRATE_MM_M { (80*60), (80*60), (6*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
